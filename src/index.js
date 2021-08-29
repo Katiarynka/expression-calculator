7f758fbfde472090ed65276f9c1d4a2951b186cc
Katiarynka <katia.tsygankova@gmail.com>
2021-08-29 18:24:04 +0300

    solve the task

--- src/index.js
+++ src/index.js
@@ -4,7 +4,91 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+   
+    let functions =
+    {
+        ['+']: (x, y) => +x + +y,
+        ['-']: (x, y) => +x - +y,
+        ['*']: (x, y) => +x * +y,
+        ['/']: (x, y) => y == '0' ? "TypeError: Division by zero." : +x / +y
+    }
+
+    let arrayRPN = [];
+    let stack = [];
+
+    let firstPriority = ['/', '*']
+    let secondPriority = ['+', '-']
+    let funcArray = ['(', ')'].concat(firstPriority).concat(secondPriority)
+
+    let number = '';
+
+    for (let i = 0; i < expr.length; i++) {
+
+        if (expr[i] === ' ') { continue }
+
+        if (funcArray.includes(expr[i])) {
+            if (number !== '') {
+                arrayRPN.push(number);
+                number = '';
+            }
+
+            while (true) {
+                if (expr[i] === ')' && stack.length === 0) {
+                    return "ExpressionError: Brackets must be paired"
+                }
+                else if (stack.length === 0
+                    || firstPriority.includes(expr[i]) && secondPriority.includes(stack[stack.length - 1])
+                    || expr[i] === '('
+                    || stack[stack.length - 1] === '(' && expr[i] !== ')') {
+                    stack.push(expr[i]);
+                    break
+                }
+                else if (expr[i] === ')' && stack[stack.length - 1] === '(') {
+                    stack.pop()
+                    break
+                }
+                else { arrayRPN.push(stack.pop()) }
+            }
+        }
+        else {
+            number = `${number}${expr[i]}`
+        }
+    }
+    
+    if (number !== '') {
+        arrayRPN.push(number);		
+    }
+
+    for (let j = stack.length; j > 0; j--) {
+        if (stack[j - 1] === '(') { return "ExpressionError: Brackets must be paired" }
+        arrayRPN.push(stack[j - 1])
+    }
+
+    //second part
+    let resultArray = []
+
+    for (let i = 0; i < arrayRPN.length; i++) {
+
+        if (funcArray.includes(arrayRPN[i])) {
+            let secondNumber = resultArray.pop();
+            let firstNumber = resultArray.pop();
+
+            let resultNumber = functions[arrayRPN[i]](firstNumber, secondNumber)
+
+            if (typeof resultNumber === "number") {
+                resultArray.push(resultNumber);
+            } else {
+                return resultNumber
+            }
+        }
+        else {
+            resultArray.push(arrayRPN[i])
+        }
+
+    }
+
+    return resultArray[0]
+
 }
 
 module.exports = {
